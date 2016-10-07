80c4ade7ad9b430cd0553eb098e399c2f67dfba2
Gabriel Credico <me@gabrielcredico.de>
2016-10-07 23:08:11 +0200

    treemap labels

--- microsite/js/container/treemap/NestedTreemap.js
+++ microsite/js/container/treemap/NestedTreemap.js
@@ -42,6 +42,13 @@ export class NestedTreemap {
 			.style("width", this.width )
 			.style("height", this.height );
 
+		this.connectionSVG = this.svg
+			.append('svg')
+			.style('position', 'absolute')
+			.attr("class", "label-connections")
+			.style("width", this.width)
+			.style("height", 62);
+
 		return this;
 	}
 
@@ -114,9 +121,9 @@ export class NestedTreemap {
 
 	setNodeDimensions (selection) {
 		selection
-			.style("transform", d => `translate3d(${d.x0}px,${d.y0}px,0px)` )
+			.style("transform", d => `translate3d(${d.x0}px,${d.y0+60}px,0px)` )
 			.style("width", d => { return ((d.x1-d.x0)+"px"); })
-			.style("height", d => ((d.y1 - d.y0)+"px"));
+			.style("height", d => (d.depth==1?(d.y1-d.y0-60):(d.y1-d.y0))+'px' );
 
 		selection.select(".label").text(d => d.depth!=0?d.data.key:null);
 		selection.select(".count").text(d => d.depth!=0?d.data.value:null);
@@ -174,9 +181,25 @@ export class NestedTreemap {
 	}
 
 
-	render(mode) {
+	render (mode) {
+
+		let line = d3.line().x(d=>Math.round(d[0])).y(d=>d[1]).curve(d3.curveMonotoneY)
+
+		function connectionLinePath (d,i,array) {
+				let shift = 30
+				let amt = i/array.length
+				let vStart = [i*w+w,0]
+				let vEnd = [d.x0+(d.x1-d.x0)*0.5,60]
+				let v0 = [vStart[0],vStart[1]]; v0[1]+=shift-(shift*amt);
+				let v1 = [vEnd[0],vEnd[1]]; v1[1]-=shift*amt;
+
+				return line( [vStart,v0,v1,vEnd] )
+			}
+
 
 		console.log(mode)
+
+		let l, w, nodes
 
 		// this filter function enables us render partial data depending on the brushevent
 		let filter = () => true;
@@ -185,29 +208,113 @@ export class NestedTreemap {
 			case "brushend" : filter = (d) => { return (d.depth > 0) }; break;
 		}
 
+
 		// data filtered depending on the brushevent
-		let data = this.root.descendants().filter(filter)
-		this.nodes = this.svg.selectAll(".node").data(data, this.dKey)
+		let data = this.root.descendants()
+		let dataLevel1 = data.filter((d)=>d.depth==1)
+		let dataLevel2 = data.filter((d)=>d.depth==2)
+
+		// this.nodes = this.svg.selectAll(".node").data(data, this.dKey)
 
 		switch (mode) {
 			case "brushstart" :
 				// update all existing nodes
-				this.nodes
-					.call(this.setNodeDimensions)
-					.style("height", "100px")
-					.call(this.setNodeID)
-					.call(this.setNodeClass);
+				// this.nodes
+				// 	.call(this.setNodeDimensions)
+				// 	.style("height", "100px")
+				// 	.call(this.setNodeID)
+				// 	.call(this.setNodeClass);
 			break;
 			case "brushmove" :
-				// update all existing nodes
-				this.nodes
+
+				this.svg.selectAll(".node.level-2").remove()
+
+				let nodesLevel1 =  this.svg.selectAll(".node.level-1").data(dataLevel1, this.dKey)
+				let labelLevel1 =  this.svg.selectAll(".level-1-label").data(dataLevel1, this.dKey)
+				let connections =  this.connectionSVG.selectAll("path").data(dataLevel1, this.dKey)
+
+				w = this.width / dataLevel1.length
+				// enter l1-labels
+				let enteredLabelLevel1 = labelLevel1.enter().append('div')
+					.classed('level-1-label', true)
+					.text((d) => d.data.key )
+					.style('width', w+'px')
+					.style('height', '60')
+					.style('transform',(d,i) => `translate3d(${i*w}px,0px,0px)`)
+				// update 
+				labelLevel1
+					.style('width', w+'px')
+					.style('transform',(d,i) => `translate3d(${i*w}px,0px,0px)`)
+				// exit
+				labelLevel1.exit().remove()
+
+
+				// enter connection lines
+				let enteredConnections = connections.enter()
+					.append('path')
+					.attr('class', (d) => ('path ' + d.data.key) )
+					.attr( "d", connectionLinePath)
+				// update
+				connections.transition().duration(300)
+					.attr( "d", connectionLinePath)
+
+				// exit
+				connections.exit().remove()
+
+				// enter l1-nodes
+				let enteredNodesLevel1 = nodesLevel1.enter().append('div')
+					.call(this.setNodeClass)
+					.call(this.setNodeID)
+					.call(this.setNodeDimensions)
+				// update 
+				nodesLevel1
 					.call(this.setNodeDimensions)
 					.call(this.setNodeID)
-					.call(this.setNodeClass)
+				// exit
+				nodesLevel1.exit().remove()
+
+
 			break;
 			case "brushend" :
 
+				let nodesLevel2 =  this.svg.selectAll(".node.level-2").data(dataLevel2, this.dKey)
+
+				// enter l2-nodes
+				let enteredNodesLevel2 = nodesLevel2.enter().append('div')
+					.call(this.setNodeClass)
+					.call(this.setNodeID)
+					.call(this.setNodeDimensions)
+						.call(this.setMouseBehaviour.bind(this))
+				// labels
+				enteredNodesLevel2
+					.append("span")
+					.classed("label", true)
+					.text(d => d.depth!=0?d.data.key:null);
+				enteredNodesLevel2
+					.append("span")
+					.classed("count", true)
+					.text(d => d.depth!=0?d.data.value:null);
+				// check for overflow
+		    enteredNodesLevel2
+		      .call( (d) => { 
+		      	d.nodes().forEach( (n) => {
+		      		this.checkOverflow.bind(n)()
+		      	})
+		      })
+		      
+				// // update 
+				// nodesLevel2
+				// 	.call(this.setNodeDimensions)
+				// 	.call(this.setNodeID)
+				// // exit
+				// nodesLevel2.exit().remove()
+
+
+
+/*
+				// update the existing level 2 nodes
 				this.nodes
+					.filter(f=>f.depth==2)
 					.call(this.setNodeID)
 					.call(this.setNodeClass)
 					.call(this.setNodeDimensions)
@@ -217,35 +324,51 @@ export class NestedTreemap {
 						.enter()
 						.append("div")
 						.call(this.setNodeClass)
-						.call(this.setNodeDimensions)
 						.call(this.setNodeID)
-						.call(this.setMouseBehaviour.bind(this));
+						.call(this.setMouseBehaviour.bind(this))
 
-				enteredNodes
+				let enteredNodesL2 = enteredNodes.filter(f=>f.depth==2)
+
+
+				// level 2 nodes follow the treemap data
+				enteredNodesL2
+					.call(this.setNodeDimensions)
+					.call(this.setMouseBehaviour.bind(this));
+
+				enteredNodesL2
 					.append("span")
 					.classed("label", true)
 					.text(d => d.depth!=0?d.data.key:null);
 
-				enteredNodes
+				enteredNodesL2
 					.append("span")
 					.classed("count", true)
 					.text(d => d.depth!=0?d.data.value:null);
 
 				// got through each new node and check if its overflowing
-		    enteredNodes
+		    enteredNodesL2
 		      .call( (d) => { 
 		      	d.nodes().forEach( (n) => {
 		      		this.checkOverflow.bind(n)()
 		      	})
 		      })
 
+
+				let nodesL1 = this.nodes.filter(f=>f.depth==1)
+
+
+				// width of the first level nodes
+				w = this.width / nodesL1.nodes().length
+
+				// level 1 nodes have a connection line and are spaced equally
+				nodesL1
+					.style('background-color', '#cecece')
+					.style('width', w)
+					.style('height', '30px')
+					.style('transform',(d,i) => `translate3d(${i*w}px,0px,0px)`)
+*/
 			break;
 		}
-
-
-		this.nodes
-			.exit()
-			.remove();
 
 		return this;
   	}
