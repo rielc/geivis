bc9d0d455308388537df66dacf9757d5c7991a8c
Marian Dörk <md@mariandoerk.de>
2017-07-31 17:51:04 +0200

    links to fhp, lab, gei, chris & gabriel

--- microsite/build.js
+++ microsite/build.js
@@ -9992,7 +9992,9 @@ $__System.register('46', ['5', '6', '15', '16', '25'], function (_export) {
 
           this.title.text('Über');
 
-          this.setIntro(['Die prototypischen Visualisierungen auf dieser Webseite sind an der Fachhochschule Potsdam im Urban Complexity Lab im Rahmen eines Forschungsprojektes mit dem Georg-Eckert-Institut für internationale Schulbuchforschung enstanden.', 'Design und Entwicklung: Christopher Pietsch & Gabriel Credico\n      <br>\nWissenschaftliche Leitung: Prof. Dr. Marian Dörk\n<br>\nWissenschaftliche Betreuung seitens des GEI: \nDr. Anke Hertling, Lena Stahn, Prof. Dr.-Ing. Ernesto William De Luca\n<br><br>\nDie Visualisierungen basieren auf GEI-Digital, Stand February 2017.']);
+					// MD: wenn ich hier etwas ändere scheint sich nix zu tun …
+
+          this.setIntro(['Die prototypischen Visualisierungen auf dieser Webseite sind an der Fachhochschule Potsdam im <a href="http://uclab.fh-potsdam.de">Urban Complexity Lab</a> im Rahmen eines Forschungsprojektes mit dem <a href="http://www.gei.de/">Georg-Eckert-Institut für internationale Schulbuchforschung</a> entstanden.', 'Design und Entwicklung: <a href="https://chrispie.com/">Christopher Pietsch</a> & <a href="http://www.gabrielcredico.de/">Gabriel Credico</a>\n      <br>\nWissenschaftliche Leitung: Prof. Dr. Marian Dörk\n<br>\nWissenschaftliche Betreuung seitens des GEI: \nDr. Anke Hertling, Lena Stahn, Prof. Dr.-Ing. Ernesto William De Luca\n<br><br>\nDie Visualisierungen basieren auf <a href="http://gei-digital.gei.de/">GEI-Digital</a>, Stand February 2017.']);
         }
 
         _createClass(AboutSection, [{
--- microsite/js/container/about/AboutSection.js
+++ microsite/js/container/about/AboutSection.js
@@ -10,15 +10,7 @@ export class AboutSection extends Section {
     this.title.text(`Über`);
 
     this.setIntro([
-      'Die prototypischen Visualisierungen auf dieser Webseite sind an der Fachhochschule Potsdam im Urban Complexity Lab im Rahmen eines Forschungsprojektes mit dem Georg-Eckert-Institut für internationale Schulbuchforschung enstanden.',
-      `Design und Entwicklung: Christopher Pietsch & Gabriel Credico
-      <br>
-Wissenschaftliche Leitung: Prof. Dr. Marian Dörk
-<br>
-Wissenschaftliche Betreuung seitens des GEI: 
-Dr. Anke Hertling, Lena Stahn, Prof. Dr.-Ing. Ernesto William De Luca
-<br><br>
-Die Visualisierungen basieren auf GEI-Digital, Stand February 2017.`,
+      'Die prototypischen Visualisierungen auf dieser Webseite sind an der <a href="http://www.fh-potsdam.de/">Fachhochschule Potsdam</a> im <a href="http://uclab.fh-potsdam.de">Urban Complexity Lab</a> im Rahmen eines Forschungsprojektes mit dem <a href="http://www.gei.de/">Georg-Eckert-Institut</a> für internationale Schulbuchforschung entstanden.', 'Design und Entwicklung: <a href="https://chrispie.com/">Christopher Pietsch</a> & <a href="http://www.gabrielcredico.de/">Gabriel Credico</a>\n      <br>\nWissenschaftliche Leitung: Prof. Dr. Marian Dörk\n<br>\nWissenschaftliche Betreuung seitens des GEI: \nDr. Anke Hertling, Lena Stahn, Prof. Dr.-Ing. Ernesto William De Luca\n<br><br>\nDie Visualisierungen basieren auf <a href="http://gei-digital.gei.de/">GEI-Digital</a>, Stand February 2017.',
       ])
   }
 
